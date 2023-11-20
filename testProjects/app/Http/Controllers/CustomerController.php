bbfbc1675b33c8e87af4d0e3661d1a5f15ac16dc
Raveen Yasintha <yasintharaveen13@gmail.com>
2023-11-20 11:06:07 +0000

    first commit

---
+++ testProjects/app/Http/Controllers/CustomerController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        return 'i am from api Controllers';
+    }
+}
